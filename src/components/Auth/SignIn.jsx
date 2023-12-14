ceffc2550cd60e16377b6837b495a5674ac56c24
Andrew Galicia <andrewlgalicia@gmail.com>
2023-12-14 13:46:12 -0800

    sign-in cleanup

--- src/components/Auth/SignIn.jsx
+++ src/components/Auth/SignIn.jsx
@@ -1,9 +1,12 @@
-import {auth, provider, signInWithGoogle} from "../../firebase"
+import './SignIn.css'
+import {signInWithGoogle, SignOutOfGoogle} from "../../firebase"
 
-export default function SignIn() {
+export default function SignInButton() {
     return (
         <div>
-            <button onClick={signInWithGoogle}>Sign in with google</button>
+            <button className='login-with-google-btn' onClick={signInWithGoogle}>Sign in with google</button>
+            
+           
         </div>
     )
 }
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import SignIn from '../../components/Auth/SignIn'
+import SignInButton from '../../components/Auth/SignIn'
 
 export default function AuthPage() {
   return (
     <div>
       <h2>Authentication Page</h2>
       {/* Add your authentication form or other content here */}
-      <SignIn/>
+      <SignInButton/>
     </div>
   );
 }
