6c6cb4f4823d3bb4069479d73daffbdd12444a7a
Andrew Galicia <andrewlgalicia@gmail.com>
2023-12-14 14:10:57 -0800

    added signout to signin component

--- src/components/Auth/SignIn.jsx
+++ src/components/Auth/SignIn.jsx
@@ -1,12 +1,34 @@
-import './SignIn.css'
-import {signInWithGoogle, SignOutOfGoogle} from "../../firebase"
+import React, { useState, useEffect } from 'react';
+import './SignIn.css';
+import { auth, signInWithGoogle, SignOutOfGoogle } from '../../firebase';
 
 export default function SignInButton() {
-    return (
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return (
+    <div>
+      {user ? (
         <div>
-            <button className='login-with-google-btn' onClick={signInWithGoogle}>Sign in with google</button>
-            
-           
+          <p>Welcome, {user.displayName}!</p>
+          <button className='sign-out-btn' onClick={SignOutOfGoogle}>
+            Sign Out
+          </button>
         </div>
-    )
+      ) : (
+        <button className='login-with-google-btn' onClick={signInWithGoogle}>
+          Sign in with Google
+        </button>
+      )}
+    </div>
+  );
 }
