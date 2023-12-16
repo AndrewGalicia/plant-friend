e478acb2619e5669dec06b3fc5de917a3dad4dc1
Andrew Galicia <andrewlgalicia@gmail.com>
2023-12-15 16:50:05 -0800

    Created dropdown menu, need to fix bug

--- src/components/Auth/SignIn.jsx
+++ src/components/Auth/SignIn.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './SignIn.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { auth, signInWithGoogle, SignOutOfGoogle } from '../../firebase';
 
 export default function SignInButton() {
@@ -18,18 +19,24 @@ export default function SignInButton() {
   return (
     <div>
       {user ? (
-        <div class="container">
-          <div class="row">
-            <p class="col">Welcome, {user.displayName}!</p>
-            <button class="col" className='sign-out-btn' onClick={SignOutOfGoogle}>
-              Sign Out
-            </button>
+        <div class="dropdown">
+          <button type="button" data-bs-toggle="dropdown" class="btn btn-primary dropdown-toggle">
+                <span class="fw-bold text-light">
+                        Menu
+                </span>
+          </button>
+          <div class="dropdown-menu">
+            <ul class="dropdown-header">
+              <li> <h6 class="dropdown-header">Welcome, {user.displayName}!</h6></li> 
+              <li> <button class="dropdown-item" onClick={SignOutOfGoogle}>Sign Out</button></li>
+            </ul>
           </div>
         </div>
       ) : (
         <button className='login-with-google-btn' onClick={signInWithGoogle}>
           Sign in with Google
         </button>
+        
       )}
     </div>
   );
