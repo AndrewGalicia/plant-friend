01e78eb3f9775adc8ad3c7ed35e3bf46f430c102
Andrew Galicia <andrewlgalicia@gmail.com>
2024-01-24 17:43:05 -0800

    fixed card

--- src/components/PlantCard/PlantCard.jsx
+++ src/components/PlantCard/PlantCard.jsx
@@ -10,7 +10,7 @@ export default function PlantCard() {
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await axios.get(`https://perenual.com/api/species/details/1?key=${PERENUAL}`);
+        const response = await axios.get(`https://perenual.com/api/species-list?key=${PERENUAL}`);
         setData(response.data);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -23,11 +23,10 @@ export default function PlantCard() {
   return (
     <div>
       {data ? (
-        <div className="card" style={{ width: '18rem' }}>
-          <img src={data.default_image.thumbnail} className="card-img-top" alt={data.common_name} />
+        <div className="card" style={{ width: '12rem' }}>
+          <img src={data.data[0].default_image.thumbnail} className="card-img-top" alt={data.data[0].common_name} />
           <div className="card-body">
-            <h5 className="card-title">{data.common_name}</h5>
-            <p className="card-text">{data.description}</p>
+            <h5 className="card-title">{data.data[0].common_name}</h5>
             <a href="#" className="btn btn-primary">
               Go somewhere
             </a>
